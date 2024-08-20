ee58fac52d1cb824a0c15dac8046204498f2f72a
hyr <huyanrui326@outlook.com>
2024-08-20 16:57:24 +0800

    score 30

--- kernel/defs.h
+++ kernel/defs.h
@@ -64,6 +64,11 @@ void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
 
+int             krefcnt(void*);
+int             cowpage(pagetable_t, uint64);
+void*           cowalloc(pagetable_t, uint64);
+int             kaddrefcnt(void*);
+
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -171,6 +176,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+
+pte_t*          walk(pagetable_t, uint64, int);
 
 // plic.c
 void            plicinit(void);
