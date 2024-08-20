41db0b1de3bc20858766f0fcaab5fad2ad0ee264
hyr <huyanrui326@outlook.com>
2024-08-20 14:10:16 +0800

    score 20 usertests fail

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -43,12 +43,25 @@ sys_sbrk(void)
 {
   int addr;
   int n;
+  uint64 sz;
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
+
+  struct proc* p = myproc();
+  addr = p->sz;
+  sz = p->sz;
+
+  // lazy allocation
+  if(n > 0)
+    p->sz += n;
+  else if(sz+n > 0){
+    sz = uvmdealloc(p->pagetable, sz, sz+n);
+    p->sz = sz;
+  }
+  else
     return -1;
+
   return addr;
 }
 
